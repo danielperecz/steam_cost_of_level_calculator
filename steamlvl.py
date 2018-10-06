7726d7df108a5c8535a100221969bca2a6b8e988
danielperecz <42898223+danielperecz@users.noreply.github.com>
2018-10-06 14:59:40 +0100

    Update steamlvl.py

--- steamlvl.py
+++ steamlvl.py
@@ -5,72 +5,79 @@ from math import ceil
 from time import sleep
 
 # Basic information printed every time
-print(	">>>Enter the following information to calculate cost of desired Steam level",
+print(">>>Enter the following information to calculate cost of desired Steam level",
 	"\n>>>Calculations are based on XP\n>>>To force exit press CTRL + C\n")
 
 # Formatting
 hyp_count = 29		# Number of hyphens printed
 right_align = 21	# Right aligning input() message
-secs = 5		# Sleep for this many seconds
+secs = 5			# Sleep for this many seconds
 
-try: # User input inside of try for exception handling
+error_msgs = 	{
+					0 : "\n>>>ERROR: minimum value is 0",
+					1 : "\n>>>ERROR: identical Current XP and Desired XP",
+					2 : "\n>>>ERROR: higher Current XP than Desired XP",
+					3 : "\n>>>ERROR: minimum value is 100",
+					4 : "\n>>>ERROR: positive value required",
+					5 : "\n>>>ERROR: minimum value is 5",
+					6 : "\n>>>ERROR: maximum value is 15",
+					7 : "\n>>>ERROR: enter a whole number",
+					8 : "\n>>>Force exited"
+				}
+
+def print_error(msg):
+	"""Prints hyphens, error messages, and takes care of stopping script execution"""
+	global secs
+	print("-" * hyp_count)
+	print(error_msgs[msg])
+	if msg == 8:
+		secs = 0			# Should have instant effect in the case of CTRL + C
+	sleep(secs), exit()
+
+try:
+	"""User input inside of try for exception handling"""
 	print("-" * hyp_count)
 	print("{:>{}}".format("INPUT", right_align + 5))
 	current_xp = int(input("{:>{}}".format("Current XP: ", right_align)))
-	# The following flow control statements are well explained by their corresponding ERROR messages
 	if current_xp < 0:
-		print("-" * hyp_count)
-		print("\n>>>ERROR: minimum value is 0")
-		sleep(secs), exit()
+		print_error(0)
 
 	desired_level_xp = int(input("{:>{}}".format("Desired XP: ", right_align)))
 	if desired_level_xp == current_xp:
-		print("-" * hyp_count)
-		print("\n>>>ERROR: identical Current XP and Desired XP")
-		sleep(secs), exit()
+		print_error(1)
+
 	elif desired_level_xp < current_xp:
-		print("-" * hyp_count)
-		print("\n>>>ERROR: higher Current XP than Desired XP")
-		sleep(secs), exit()
+		print_error(2)
+
 	elif desired_level_xp < 100:
-		print("-" * hyp_count)
-		print("\n>>>ERROR: minimum value is 100")
-		sleep(secs), exit()
+		print_error(3)
 
 	card_cost = round(float(input("{:>{}}".format("Cost of one card: ", right_align))), 2)
 	if card_cost <= 0:
-		print("-" * hyp_count)
-		print("\n>>>ERROR: positive value required")
-		sleep(secs), exit()
+		print_error(4)
 
 	num_of_cards = int(input("{:>{}}".format("Cards in badge: ", right_align)))
 	if num_of_cards < 5:
-		print("-" * hyp_count)
-		print("\n>>>ERROR: minimum value is 5")
-		sleep(secs), exit()
+		print_error(5)
+
 	elif 15 < num_of_cards:
-		print("-" * hyp_count)
-		print("\n>>>ERROR: maximum value is 15")
-		sleep(secs), exit()
+		print_error(6)
 
 except ValueError:
-	print("-" * hyp_count)
-	print("\n>>>ERROR: enter a whole number")
-	sleep(secs), exit()
+	print_error(7)
 except KeyboardInterrupt:
-	print()	# Needed for newline character, without this the hyphens are printed in the input() line
-	print("-" * hyp_count)
-	print("\n>>>Force exited")
-	exit() # No sleep() here on purpose -- CTRL + C should have an instant effect
+	print()								# Print new line character, else hyphens printed in input line
+	print_error(8)
 
-def roundup(xp):				# Round up by 100 because one badge equals 100 XP
+def roundup(xp):						# Round up by 100 because one badge equals 100 XP
 	return int(ceil(xp / 100.0)) * 100 	# Example: you have 25 XP and want to reach level 1 (100 XP),
-						#          you therefore need one badge. Since getting a fraction of
-						#          a badge is not possible, you need the whole badge.
-						# Not taking this into account would result in an incorrect
-						# amount of badges needed (i.e. n-1).
+										#          you therefore need one badge. Since getting a fraction of
+										#          a badge is not possible, you need the whole badge.
+										# Not taking this into account would result in an incorrect
+										# amount of badges needed (i.e. n-1).
 
-def main(): # All of the calculations take place inside of main() so that it's easier to see what's going on
+def main():
+	""""All of the calculations take place inside of main(), so that it's easier to see what's going on"""
 	xp_needed = (desired_level_xp - current_xp)
 	num_of_badges_needed = (roundup(xp_needed) / 100)
 	badge_cost = float(card_cost * num_of_cards)
